7eac63132ab89c0a90ea6ba72c2133aae35646d0
Kamalesh79 <65053627+Kamalesh79@users.noreply.github.com>
2020-06-11 18:21:44 -0700

    Commit Test 1

--- app/src/main/java/com/example/continuoustempsensor/fragment_tab1.java
+++ app/src/main/java/com/example/continuoustempsensor/fragment_tab1.java
@@ -15,5 +15,6 @@ public class fragment_tab1 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tab1_layout, container, false);
+    x + 2
     }
 }
