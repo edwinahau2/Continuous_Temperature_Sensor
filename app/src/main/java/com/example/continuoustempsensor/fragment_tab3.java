8ccca873bef1eaa24dc38844a239a5e225421ca5
aryana2 <65053678+aryana2@users.noreply.github.com>
2020-07-04 18:44:32 -0700

    im trying

--- app/src/main/java/com/example/continuoustempsensor/fragment_tab3.java
+++ app/src/main/java/com/example/continuoustempsensor/fragment_tab3.java
@@ -2,19 +2,25 @@ package com.example.continuoustempsensor;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.Set;
 
 public class fragment_tab3 extends Fragment {
     private static final int RESULT_OK = -1;
@@ -24,6 +30,10 @@ public class fragment_tab3 extends Fragment {
     private ImageView mBlueIv;
     private static final int REQUEST_ENABLE_BT = 0;
     private static final int REQUEST_DISCOVER_BT = 1;
+    private boolean mScanning;
+    private Handler handler;
+    private static final long SCAN_PERIOD = 60000;
+    private LeDeviceListAdapter mLeDeviceListAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -36,6 +46,7 @@ public class fragment_tab3 extends Fragment {
         buttonDisc = view.findViewById(R.id.discoverableBtn);
         buttonFind = view.findViewById(R.id.pairedBtn);
         mBlueAdapter = BluetoothAdapter.getDefaultAdapter();
+
         if (mBlueAdapter == null) {
             mStatusBlueTv.setText("Bluetooth is not available");
         }
@@ -61,6 +72,8 @@ public class fragment_tab3 extends Fragment {
             public void onClick(View v) {
                 if (mBlueAdapter.isEnabled()){
                     mBlueAdapter.disable();
+                    mScanning = false;
+                    mBlueAdapter.stopLeScan(BluetoothAdapter.LeScanCallback);
                     Toast.makeText(getActivity(), "Turning Bluetooth Off...", Toast.LENGTH_SHORT).show();
                 }
                 else {
@@ -71,20 +84,33 @@ public class fragment_tab3 extends Fragment {
         buttonDisc.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-
+                if (!mBlueAdapter.isDiscovering()) {
+                    Toast.makeText(getActivity(), "Making Your Device Discoverable", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                    startActivityForResult(intent, REQUEST_DISCOVER_BT);
+                }
             }
         });
         buttonFind.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                if (mBlueAdapter.isDiscovering()){
-                    Toast.makeText(getActivity(), "Canceling discovery...", Toast.LENGTH_SHORT).show();
-                    mBlueAdapter.cancelDiscovery();
+                if (!mBlueAdapter.isEnabled()) {
+                    Toast.makeText(getActivity(), "Turning on Bluetooth...", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intent, REQUEST_ENABLE_BT);
                 }
                 else {
                     Toast.makeText(getActivity(), "Finding Devices...", Toast.LENGTH_SHORT).show();
-                    mBlueAdapter.startDiscovery();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mScanning = false;
+                            mBlueAdapter.stopLeScan(BluetoothAdapter.LeScanCallback);
+                        }
+                    }, SCAN_PERIOD);
+                    mScanning = true;
+                    mBlueAdapter.startLeScan(BluetoothAdapter.LeScanCallback);
                 }
             }
         });
@@ -101,5 +127,8 @@ public class fragment_tab3 extends Fragment {
                 }
                 break;
         }
+    }
+
+
     }
 }
