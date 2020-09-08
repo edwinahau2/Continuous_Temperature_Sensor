ae285394b6f2037377b87ce581290dbd9fc18b18
edwinahau2 <65053616+edwinahau2@users.noreply.github.com>
2020-09-08 16:53:37 -0700

    moved code to fragment tab 1. Layout fixed.

--- app/src/main/java/com/example/continuoustempsensor/MainActivity.java
+++ app/src/main/java/com/example/continuoustempsensor/MainActivity.java
@@ -1,29 +1,16 @@
 package com.example.continuoustempsensor;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.continuoustempsensor.ui.main.SectionsPagerAdapter;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 
 
 public class MainActivity extends AppCompatActivity {
-    private NotificationManagerCompat notificationManager;
-    private EditText editTextTitle;
-    private EditText editTextMessage;
-
-   // Button notify_button;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,65 +20,5 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        notificationManager = NotificationManagerCompat.from(this);
-
-        editTextTitle = findViewByID(R.edit_text_title);
-        editTextMessage = findViewById(R.id.edit_text_message);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-//        notify_button = findViewById(R.id.push);
-//        notify_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //code that appears in the notification
-//                NotificationCompat.Builder mbuilder = (NotificationCompat.Builder)
-//                        new NotificationCompat.Builder(getApplicationContext())
-//                                .setSmallIcon(R.drawable.warning)
-//                                .setContentTitle("Notification")
-//                                .setContentText("you have been warned lol");
-//
-//                NotificationManager notificationManager = (NotificationManager)
-//                        getSystemService(NOTIFICATION_SERVICE);
-//                notificationManager.notify(0, mbuilder.build());
-//            }
-//        });
     }
-
-    public void sendUrgentWarning1(View view) {
-        String title = editTextTitle.getText().toString();
-        String message = editTextMessage.getText().toString();
-        android.app.Notification notification = new NotificationCompat.Builder(this, notifications.CHANNEL_1_ID)
-                .setSmallIcon(R.drawable.warning)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_ALARM)
-                .build();
-        notificationManager.notify(1,notification);
-    }
-
-    public void sendUpdate2(View view) {
-        String title = editTextTitle.getText().toString();
-        String message = editTextMessage.getText().toString();
-        android.app.Notification notification = new NotificationCompat.Builder(this, notifications.CHANNEL_2_ID)
-                .setSmallIcon(R.drawable.announcement)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setCategory(NotificationCompat.CATEGORY_EVENT)
-                .build();
-        notificationManager.notify(2,notification);
-    }
-
-
 }
--- app/src/main/java/com/example/continuoustempsensor/fragment_tab1.java
+++ app/src/main/java/com/example/continuoustempsensor/fragment_tab1.java
@@ -4,41 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
 public class fragment_tab1 extends Fragment {
+    private NotificationManagerCompat notificationManager;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab1_layout, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab1_layout, container, false);
 
-//        public void sendUrgentWarning1(View sendUrgentWarning1) {
-//            String title = editTextTitle.getText().toString();
-//            String message = editTextMessage.getText().toString();
-//            android.app.Notification notification = new NotificationCompat.Builder(this, Notification.CHANNEL_1_ID)
-//                    .setSmallIcon(R.drawable.warning)
-//                    .setContentTitle(title)
-//                    .setContentText(message)
-//                    .setPriority(NotificationCompat.PRIORITY_HIGH)
-//                    .setCategory(NotificationCompat.CATEGORY_ALARM)
-//                    .build();
-//            notificationManager.notify(1,notification);
-//        }
-//
-//        public void sendUpdate2(View sendUpdate2) {
-//            String title = editTextTitle.getText().toString();
-//            String message = editTextMessage.getText().toString();
-//            android.app.Notification notification = new NotificationCompat.Builder(this, Notification.CHANNEL_2_ID)
-//                    .setSmallIcon(R.drawable.ic_announcement_black_24dp)
-//                    .setContentTitle(title)
-//                    .setContentText(message)
-//                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-//                    .setCategory(NotificationCompat.CATEGORY_EVENT)
-//                    .build();
-//            notificationManager.notify(2,notification);
-//        }
+        notificationManager = NotificationManagerCompat.from(this.getContext());
+        editTextTitle = view.findViewById(R.id.edit_text_title);
+        editTextMessage = view.findViewById(R.id.edit_text_message);
+
+        return view;
+    }
+
+    public void sendUrgentWarning1(View view){
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+        android.app.Notification notification = new NotificationCompat.Builder(this.getContext(), notifications.CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.warning)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_ALARM)
+                .build();
+        notificationManager.notify(1, notification);
+    }
+
+    public void sendUpdate2(View view){
+        String title = editTextTitle.getText().toString();
+        String message = editTextMessage.getText().toString();
+        android.app.Notification notification = new NotificationCompat.Builder(this.getContext(), notifications.CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.announcement)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_EVENT)
+                .build();
+        notificationManager.notify(2, notification);
     }
 }
