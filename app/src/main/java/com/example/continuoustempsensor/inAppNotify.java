079f553473e7a611366738896b63974871c311cc
edwinahau2 <65053616+edwinahau2@users.noreply.github.com>
2020-10-06 19:42:22 -0700

    Wrote out code for inAppNotify class. Need to include the interval time for in app notifcations as specified in Aryan's code.

--- app/src/main/java/com/example/continuoustempsensor/inAppNotify.java
+++ app/src/main/java/com/example/continuoustempsensor/inAppNotify.java
@@ -5,15 +5,21 @@ import android.content.Intent;
 
 public class inAppNotify {
     private int color;
-    public String time;
+    private String timeNotification;
+    private String interval;
+    private String text;
+    private String generalText;
 
     inAppNotify(Intent intent, Context context, int code) {
+        interval = ; //the interval of time for which each notification is sent in app (selected by the user)
+        generalText = "Continue to physically distance yourself from others at 6 feet minimum and wear a face covering. Tap here more information.";
+
         if (code == 0) {
-
+            text = timeNotification + "\nYou're temperatures have been normal for the past " + interval + ". " + generalText;
         } else if (code == 1) {
-
+            text = timeNotification + "\nYou're temperature readings have been trending upwards. Other symptoms of Coronavirus include ___, as stated by the CDC. " + generalText;
         } else if (code == 2) {
-
+            text = timeNotification + "\nYou're temperature readings indicate you are having a fever. This is a symptom of Coronavirus. Please isolate yourself immediately and contact your supervisor for further instructions. " + generalText;
         }
     }
 
@@ -21,7 +27,19 @@ public class inAppNotify {
         return color;
     }
 
-    public String getTime() {
-        return time;
+    public String getInterval() {
+        return interval;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTimeNotification(){
+        return timeNotification;
+    }
+
+    public String getGeneralText(){
+        return generalText;
     }
 }
